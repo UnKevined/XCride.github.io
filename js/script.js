66828ec6001d837924d5d9e064113765a38a4b62
UnKevined <kevin.jurina@googlemail.com>
2023-06-06 16:08:14 +0200

    Update59

--- js/script.js
+++ js/script.js
@@ -1,140 +1,3 @@
-
-/*
-$(window).on('resize load', function() {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    var aspectRatio = windowWidth / windowHeight;
-  
-    if (aspectRatio > 1) {
-      $('.background-image').css({
-        'width': '100%',
-        'height': 'auto'
-      });
-    } else {
-      $('.background-image').css({
-        'width': 'auto',
-        'height': '100%'
-      });
-    }
-  });
-  
-  $(document).ready(function() {
-    $(window).scroll(function() {
-      var scrollTop = $(window).scrollTop();
-      var windowHeight = $(window).height();
-  
-      $('.background-image').each(function() {
-        var imageTop = $(this).offset().top;
-        
-        if (imageTop < scrollTop + windowHeight && imageTop + $(this).height() > scrollTop) {
-          $(this).addClass('active');
-        } else {
-          $(this).removeClass('active');
-        }
-      });
-    });
-  });
-  
-
-
-
-
-  $(window).on('resize load', function() {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    var aspectRatio = windowWidth / windowHeight;
-  
-    if (aspectRatio > 1) {
-      $('.background-image').css({
-        'width': '100%',
-        'height': 'auto'
-      });
-    } else {
-      $('.background-image').css({
-        'width': 'auto',
-        'height': '100%'
-      });
-    }
-  });
-  
-  $(document).ready(function() {
-    $(window).scroll(function() {
-      var scrollTop = $(window).scrollTop();
-      var windowHeight = $(window).height();
-  
-      $('.background-image').each(function() {
-        var imageTop = $(this).offset().top;
-        var imageHeight = $(this).height();
-        
-        if (imageTop + imageHeight > scrollTop && imageTop < scrollTop + windowHeight) {
-          $(this).addClass('active');
-        } else {
-          $(this).removeClass('active');
-        }
-      });
-    });
-  });
-  
-
-
-
-
-
-
-  
-    $(window).on('resize load', function() {
-      var windowWidth = $(window).width();
-      var windowHeight = $(window).height();
-      var aspectRatio = windowWidth / windowHeight;
-
-      if (aspectRatio > 1) {
-        $('.background-image').css({
-          'width': '100%',
-          'height': 'auto'
-        });
-      } else {
-        $('.background-image').css({
-          'width': 'auto',
-          'height': '100%'
-        });
-      }
-    });
-
-    $(document).ready(function() {
-      var backgroundImages = $('.background-image');
-      var totalImages = backgroundImages.length;
-      var activeImageIndex = 0;
-
-      function switchBackgroundImage() {
-        backgroundImages.removeClass('active');
-        $(backgroundImages[activeImageIndex]).addClass('active');
-        activeImageIndex = (activeImageIndex + 1) % totalImages;
-      }
-
-      var interval = 5000;
-      setInterval(switchBackgroundImage, interval);
-
-      $(window).scroll(function() {
-        var scrollTop = $(window).scrollTop();
-        var windowHeight = $(window).height();
-
-        backgroundImages.each(function() {
-          var imageTop = $(this).offset().top;
-          var imageHeight = $(this).height();
-      
-          if (imageTop + imageHeight > scrollTop && imageTop < scrollTop + windowHeight) {
-            $(this).addClass('active');
-          } else {
-            $(this).removeClass('active');
-          }
-        });
-      });
-    });
-
-
-
-*/
-
 $(window).on('resize load', function() {
   var windowWidth = $(window).width();
   var windowHeight = $(window).height();
@@ -167,6 +30,19 @@ $(document).ready(function() {
   var scrollThreshold = 0.8; // Adjust the scroll threshold as needed
   var scrollFlag = false;
 
+  $('#comment-form').submit(function(e) {
+    e.preventDefault();
+    var name = $('#name').val();
+    var comment = $('#comment').val();
+
+    // Füge den Kommentar der Liste hinzu
+    $('#comment-list').append('<li><strong>' + name + ':</strong> ' + comment + '</li>');
+
+    // Zurücksetzen der Formularfelder
+    $('#name').val('');
+    $('#comment').val('');
+  });
+
   $(window).scroll(function() {
     if (!scrollFlag) {
       scrollFlag = true;
@@ -185,4 +61,4 @@ $(document).ready(function() {
   });
 });
 
-    
+
