8897305a475ab4ccea98562d4257c07b9d728519
UnKevined <kevin.jurina@googlemail.com>
2023-06-06 15:33:10 +0200

    Update58

--- js/script.js
+++ js/script.js
@@ -135,43 +135,54 @@ $(window).on('resize load', function() {
 
 */
 
+$(window).on('resize load', function() {
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+  var aspectRatio = windowWidth / windowHeight;
 
-
-    $(window).on('resize load', function() {
-      var windowWidth = $(window).width();
-      var windowHeight = $(window).height();
-      var aspectRatio = windowWidth / windowHeight;
-    
-      if (aspectRatio > 1) {
-        $('.background-image').css({
-          'width': '100%',
-          'height': 'auto'
-        });
-      } else {
-        $('.background-image').css({
-          'width': 'auto',
-          'height': '100%'
-        });
-      }
+  if (aspectRatio > 1) {
+    $('.background-image').css({
+      'width': '100%',
+      'height': 'auto'
     });
-    
-    $(document).ready(function() {
-      var backgroundImages = $('.background-image');
-    
-      $(window).scroll(function() {
+  } else {
+    $('.background-image').css({
+      'width': 'auto',
+      'height': '100%'
+    });
+  }
+});
+
+$(document).ready(function() {
+  var backgroundImages = $('.background-image');
+  var totalImages = backgroundImages.length;
+  var activeImageIndex = 0;
+
+  function switchBackgroundImage() {
+    backgroundImages.removeClass('active');
+    $(backgroundImages[activeImageIndex]).addClass('active');
+    activeImageIndex = (activeImageIndex + 1) % totalImages;
+  }
+
+  var scrollThreshold = 0.8; // Adjust the scroll threshold as needed
+  var scrollFlag = false;
+
+  $(window).scroll(function() {
+    if (!scrollFlag) {
+      scrollFlag = true;
+      setTimeout(function() {
         var scrollTop = $(window).scrollTop();
         var windowHeight = $(window).height();
-    
-        backgroundImages.each(function() {
-          var imageTop = $(this).offset().top;
-          var imageHeight = $(this).height();
-    
-          if (imageTop + imageHeight > scrollTop && imageTop < scrollTop + windowHeight) {
-            $(this).addClass('active');
-          } else {
-            $(this).removeClass('active');
-          }
-        });
-      });
-    });
+        var scrollBottom = scrollTop + windowHeight;
+
+        if (scrollBottom >= $(document).height() * scrollThreshold) {
+          switchBackgroundImage();
+        }
+
+        scrollFlag = false;
+      }, 200);
+    }
+  });
+});
+
     
