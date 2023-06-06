3a4cb990a6573d0461d19ad7ae9b473ac02f6ec1
UnKevined <kevin.jurina@googlemail.com>
2023-06-06 13:29:06 +0200

    Update10

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-$(window).on('resize load', function() {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
-    var aspectRatio = windowWidth / windowHeight;
+// $(window).on('resize load', function() {
+//     var windowWidth = $(window).width();
+//     var windowHeight = $(window).height();
+//     var aspectRatio = windowWidth / windowHeight;
     
-    if (aspectRatio > 1) {
-      $('.background-image').css({
-        'width': '100%',
-        'height': 'auto'
+//     if (aspectRatio > 1) {
+//       $('.background-image').css({
+//         'width': '100%',
+//         'height': 'auto'
+//       });
+//     } else {
+//       $('.background-image').css({
+//         'width': 'auto',
+//         'height': '100%'
+//       });
+//     }
+//   });
+
+
+
+
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      var scrollTop = $(window).scrollTop();
+      var windowHeight = $(window).height();
+  
+      $('.background-image').each(function() {
+        var imageTop = $(this).offset().top;
+        
+        if (imageTop < scrollTop + windowHeight && imageTop + $(this).height() > scrollTop) {
+          $(this).addClass('active');
+        } else {
+          $(this).removeClass('active');
+        }
       });
-    } else {
-      $('.background-image').css({
-        'width': 'auto',
-        'height': '100%'
-      });
-    }
+    });
   });
+  
   
