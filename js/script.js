dae57ab26bdf32ff26ed06c8e97fc77c1b86bb14
UnKevined <kevin.jurina@googlemail.com>
2023-06-06 14:00:55 +0200

    Update12

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-// $(window).on('resize load', function() {
-//     var windowWidth = $(window).width();
-//     var windowHeight = $(window).height();
-//     var aspectRatio = windowWidth / windowHeight;
-    
-//     if (aspectRatio > 1) {
-//       $('.background-image').css({
-//         'width': '100%',
-//         'height': 'auto'
-//       });
-//     } else {
-//       $('.background-image').css({
-//         'width': 'auto',
-//         'height': '100%'
-//       });
-//     }
-//   });
-
-
-
-
-
+$(window).on('resize load', function() {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var aspectRatio = windowWidth / windowHeight;
+  
+    if (aspectRatio > 1) {
+      $('.background-image').css({
+        'width': '100%',
+        'height': 'auto'
+      });
+    } else {
+      $('.background-image').css({
+        'width': 'auto',
+        'height': '100%'
+      });
+    }
+  });
+  
   $(document).ready(function() {
     $(window).scroll(function() {
       var scrollTop = $(window).scrollTop();
@@ -36,5 +32,4 @@
       });
     });
   });
-  
   
